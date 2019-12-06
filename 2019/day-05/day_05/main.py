5661a1178a5ab5e78c2292fb0504e8903fc80133
Steven Powell <princeodd@gmail.com>
2019-12-06 09:22:59 -0600

    scaffold 2019d5

---
+++ 2019/day-05/day_05/main.py
@@ -0,0 +1,8 @@
+def part1():
+    print("part1")
+    pass
+
+
+def part2():
+    print("part2")
+    pass
--- 2019/day_05/test_map.py
+++
@@ -1,6 +0,0 @@
-OPERATION_MAPPING = {
-    1: addition(num1, num2),
-    2: multiplication(num1, num2)
-}
-
-total = OPERATION_MAPPING.get(1)
