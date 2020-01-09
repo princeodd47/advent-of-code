fad57d84ece6505adbdeabba9dab72742311ea6e
Steven Powell <steven.powell@adtran.com>
2020-01-09 20:45:36 +0000

    day_06 works but not tests

--- aoc2019/aoc2019/day_06.py
+++ aoc2019/aoc2019/day_06.py
@@ -39,24 +39,32 @@ class OrbitalMap():
             self._memo_list.append(satellite)
             self.get_orbits_to_com(primary)
 
-    def find_first_intersect(self, obj1, obj2):
+    def get_distance_between_objects(self, obj1, obj2):
         obj1_orbits = []
         obj2_orbits = []
-
-        obj1_primary = self._map[obj1]
-        obj2_primary = self._map[obj2]
+        if obj1 != "COM":
+            obj1_primary = self._map[obj1]
+        else:
+            obj1_primary = obj1
+        if obj2 != "COM":
+            obj2_primary = self._map[obj2]
+        else:
+            obj2_primary = obj2
 
         while obj1_primary not in obj2_orbits and obj2_primary not in obj1_orbits:
-            obj1_orbits.append(obj1_primary)
-            obj2_orbits.append(obj2_primary)
-            obj1_primary = self._map[obj1_primary]
-            obj2_primary = self._map[obj2_primary]
+            if obj1_primary != "COM":
+                obj1_orbits.append(obj1_primary)
+                obj1_primary = self._map[obj1_primary]
+            if obj2_primary != "COM":
+                obj2_orbits.append(obj2_primary)
+                obj2_primary = self._map[obj2_primary]
         if obj1_primary in obj2_orbits:
             intersect = obj1_primary
+            distance = obj2_orbits.index(intersect) + len(obj1_orbits)
         else:
             intersect = obj2_primary
-        # new intersect is not added to both lists at this point.
-        return (intersect, obj1_orbits.index(intersect), obj2_orbits.index(intersect))
+            distance = obj1_orbits.index(intersect) + len(obj2_orbits)
+        return distance
 
 
 def part1():
@@ -64,20 +72,21 @@ def part1():
     return orbital_map.get_total_orbits("COM")
 
 def part2():
-    orbital_map = OrbitalMap("input/day_06_p2_ex_1")
-    # 2 Solutions:
-    # 1 - Find path to COM for YOU and SAN, then find common, then add distances
-    # 2 - Walk back from YOU and SAN until meet, adding distance at each iteration
-    orbital_map.get_orbits_to_com("YOU")
-    you_orbits = orbital_map._memo_list
-    print(f"{you_orbits=}")
-    print(len(you_orbits))
-    orbital_map._memo_list.clear()
-    orbital_map.get_orbits_to_com("SAN")
-    san_orbits = orbital_map._memo_list
-    print(f"{san_orbits=}")
-    print(len(san_orbits))
-    orbital_map._memo_list.clear()
+    # orbital_map = OrbitalMap("input/day_06_p2_ex_1")
+    # orbital_map = OrbitalMap("input/day_06")
+    # # Solution 1 - Find path to COM for YOU and SAN, then find common, then add distances
+    # orbital_map.get_orbits_to_com("YOU")
+    # you_orbits = orbital_map._memo_list
+    # # print(f"{you_orbits=}")
+    # print(len(you_orbits))
+    # orbital_map._memo_list.clear()
+    # orbital_map.get_orbits_to_com("SAN")
+    # san_orbits = orbital_map._memo_list
+    # # print(f"{san_orbits=}")
+    # print(len(san_orbits))
+    # orbital_map._memo_list.clear()
 
-    intersect_data = orbital_map.find_first_intersect("YOU", "SAN")
-    print(f"{intersect}")
+    # Solution 2 - Walk back from YOU and SAN until meet, adding distance at each iteration
+    orbital_map = OrbitalMap("input/day_06")
+    distance = orbital_map.get_distance_between_objects("YOU", "SAN")
+    print(f"{distance=}")
--- aoc2019/tests/test_day_06.py
+++ aoc2019/tests/test_day_06.py
@@ -13,3 +13,15 @@ from aoc2019 import day_06
 def test_part1(input_file, expected_result):
     orbital_map = day_06.OrbitalMap(input_file)
     assert orbital_map.get_total_orbits("COM") == expected_result
+
+
+@pytest.mark.parametrize(
+    ("input_file", "expected_result"),
+    [
+        ("input/day_06_p2_ex_1", 4),
+        ("input/day_06", 397)
+    ]
+)
+def test_part2(input_file, expected_result):
+    orbital_map = day_06.OrbitalMap(input_file)
+    assert orbital_map.get_distance_between_objects("COM", "SAN") == expected_result
