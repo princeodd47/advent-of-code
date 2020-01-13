0671f165ecbf3486e11f5542207354aa4058936b
Steven Powell <princeodd@gmail.com>
2020-01-13 13:49:52 -0600

    day_07 examples work

--- aoc2019/aoc2019/day_07.py
+++ aoc2019/aoc2019/day_07.py
@@ -1,5 +1,48 @@
+from aoc2019.intcode import IntCode
+
+
 def part1():
-    pass
+    # input_file = "input/day_07"
+    # phase_seq = "43210"
+    # input_file = "input/day_07_p1_ex1"
+
+    # phase_seq = "01234"
+    # input_file = "input/day_07_p1_ex2"
+
+    phase_seq = "10432"
+    input_file = "input/day_07_p1_ex3"
+
+    amp_a = IntCode()
+    amp_a.get_input(input_file)
+    amp_a.set_user_input([phase_seq[0], 0])
+    amp_a.diagnostic_program()
+    result_a = amp_a.result
+
+    amp_b = IntCode()
+    amp_b.get_input(input_file)
+    amp_b.set_user_input([phase_seq[1], result_a])
+    amp_b.diagnostic_program()
+    result_b = amp_b.result
+
+    amp_c = IntCode()
+    amp_c.get_input(input_file)
+    amp_c.set_user_input([phase_seq[2], result_b])
+    amp_c.diagnostic_program()
+    result_c = amp_c.result
+
+    amp_d = IntCode()
+    amp_d.get_input(input_file)
+    amp_d.set_user_input([phase_seq[3], result_c])
+    amp_d.diagnostic_program()
+    result_d = amp_d.result
+
+    amp_e = IntCode()
+    amp_e.get_input(input_file)
+    amp_e.set_user_input([phase_seq[4], result_d])
+    amp_e.diagnostic_program()
+    result_e = amp_e.result
+
+    print(result_e)
 
 
 def part2():
