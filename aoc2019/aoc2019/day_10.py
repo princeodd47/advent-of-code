45ceed3fddb5215876ac89e36eb6f522bf189596
Steven Powell <princeodd@gmail.com>
2020-01-22 14:58:26 +0000

    add tests for d10p1

--- aoc2019/aoc2019/day_10.py
+++ aoc2019/aoc2019/day_10.py
@@ -3,13 +3,12 @@ import math
 from aoc2019 import point
 
 
-def part1():
+def part1(input_file="input/day_10_p1_ex2"):
     best_point = {
         "coordinates": point.Point(0, 0),
         "visible_points": 0
     }
-    # points = point.get_points_from_file("input/day_10_p1_ex1", "#")
-    points = point.get_points_from_file("input/day_10", "#")
+    points = point.get_points_from_file(input_file, "#")
     for initial_point in points:
         terminal_angles = set()
         for terminal_point in points:
@@ -22,7 +21,8 @@ def part1():
         if visible_points > best_point['visible_points']:
             best_point['coordinates'] = initial_point
             best_point['visible_points'] = visible_points
-    print(best_point)
+    print(best_point['coordinates'], best_point['visible_points'])
+    return best_point['coordinates'], best_point['visible_points']
 
 
 def part2():
