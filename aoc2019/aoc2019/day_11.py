3d3a86856f07f6c44ba3f13745bc40f6088b13b7
Steven Powell <princeodd@gmail.com>
2020-05-26 12:59:12 +0000

    scaffold d11

---
+++ aoc2019/aoc2019/day_11.py
@@ -0,0 +1,7 @@
+from .intcode import IntCode
+
+
+def part1():
+    intcomp = IntCode()
+    intcomp.get_input("input/day_11")
+    intcomp.set_user_input([1])
---
+++ aoc2019/tests/test_day_11.py
@@ -0,0 +1,7 @@
+import pytest
+
+from aoc2019 import day_11
+
+
+def test_part1():
+    pass
