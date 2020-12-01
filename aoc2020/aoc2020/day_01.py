8b7f094842851ef91a49716efc2e3b56db174506
Steven Powell <princeodd@gmail.com>
2020-12-01 14:55:49 +0000

    use itertools

--- aoc2020/aoc2020/day_01.py
+++ aoc2020/aoc2020/day_01.py
@@ -1,3 +1,5 @@
+import itertools
+
 from .common import get_input
 
 
@@ -11,18 +13,47 @@ def part2():
 
 def find_values_p1(input_file):
     values = get_input(input_file)
+    for a, b in itertools.combinations(values, 2):
+        if a + b == 2020:
+            product = a * b
+            return f'{a=} {b=} {product=}'
+    return f'not found'
+    # a=1301 b=719 product=935419
+
+
+def find_values_p2(input_file):
+    values = get_input(input_file)
+    for a, b, c in itertools.combinations(values, 3):
+        if a + b + c == 2020:
+            product = a * b * c
+            return f'{a=} {b=} {c=} {product=}'
+    return f'not found'
+    # a=889 b=1079 c=52 product=49880012
+
+
+def part2_brute_force():
+    print(find_values_p2_brute_force("input/day_01"))
+
+
+def part1_brute_force():
+    print(find_values_p1_brute_force("input/day_01"))
+
+
+
+def find_values_p1_brute_force(input_file):
+    values = get_input(input_file)
     for value1 in values:
         for value2 in values:
             if value1 != value2:
                 total = value1 + value2
                 if total == 2020:
                     product = value1 * value2
-                        return f'{value1=} {value2=} {product=}'
-
+                    return f'{value1=} {value2=} {product=}'
     return 'none found'
     # value1=1301 value2=719 product=935419
 
-def find_values_p2(input_file):
+
+def find_values_p2_brute_force(input_file):
     values = get_input(input_file)
     for value1 in values:
         for value2 in values:
