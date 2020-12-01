ba641f44f3ab76dcb6781e56e3cdbbbfabf45145
Steven Powell <princeodd@gmail.com>
2020-12-01 14:09:34 +0000

    brute force day1

--- aoc2020/aoc2020/day_01.py
+++ aoc2020/aoc2020/day_01.py
@@ -1,5 +1,39 @@
-# import ..common.common
+from .common import get_input
 
 
 def part1():
-    print("day 1")
+    print(find_values_p1("input/day_01"))
+
+
+def part2():
+    print(find_values_p2("input/day_01"))
+
+
+def find_values_p1(input_file):
+    values = get_input(input_file)
+    for value1 in values:
+        for value2 in values:
+            if value1 != value2:
+                total = value1 + value2
+                if total == 2020:
+                    product = value1 * value2
+                        return f'{value1=} {value2=} {product=}'
+
+    return 'none found'
+    # value1=1301 value2=719 product=935419
+
+def find_values_p2(input_file):
+    values = get_input(input_file)
+    for value1 in values:
+        for value2 in values:
+            for value3 in values:
+                if (value1 != value2
+                        and value1 != value3
+                        and value2 != value3):
+                    total = value1 + value2 + value3
+                    if total == 2020:
+                        product = value1 * value2 * value3
+                        return f'{value1=} {value2=} {value3=} {product=}'
+
+    return 'none found'
+    # value1=889 value2=1079 value3=52 product=49880012
