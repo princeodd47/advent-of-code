dcf60e6b0608cf8141e0712e2723e0440d4f33d6
princeodd47 <princeodd@gmail.com>
2020-12-01 21:21:17 -0600

    (feature): Make common project to share code (#18)
    Don't want to have multiple copies of commonly used modules. Made common
    a poetry project and added it to aoc2020 project.

--- aoc2020/aoc2020/common.py
+++
@@ -1,8 +0,0 @@
-def get_input(input_file):
-    with open(input_file, 'r') as fh:
-        return [int(line) for line in fh.readlines()]
-
-
-def get_input_as_strings(input_file):
-    with open(input_file, 'r') as fh:
-        return [line.rstrip() for line in fh.readlines()]
