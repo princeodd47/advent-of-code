065caea8a812a8f1fd45e26aa226eb58c7fc6887
Steven Powell <princeodd@gmail.com>
2020-12-04 22:31:06 +0000

    20d2p2 not working

--- aoc2020/aoc2020/day_04.py
+++ aoc2020/aoc2020/day_04.py
@@ -1,4 +1,4 @@
-import ast
+import re
 
 from common.read_file import get_input_as_strings
 
@@ -13,6 +13,8 @@ class Passport():
         self.ecl = None
         self.pid = None
         self.cid = None
+        self.hcl_string = None
+
         for key, value in dictionary.items():
             if key == "byr":
                 self.byr = value
@@ -24,6 +26,8 @@ class Passport():
                 self.hgt = value
             if key == "hcl":
                 self.hcl = value
+                if self.hcl:
+                    self.hcl_string = self.hcl[1:]
             if key == "ecl":
                 self.ecl = value
             if key == "pid":
@@ -44,49 +48,95 @@ class Passport():
         return all(conditions)
 
     def valid_byr(self):
-        if self.byr is None:
-            return False
-        return True
+        if self.byr:
+            byr_int = int(self.byr)
+        else:
+            byr_int = 0
+        conditions = [
+            self.byr,
+            1920 <= byr_int <= 2002
+        ]
+        return all(conditions)
 
     def valid_iyr(self):
-        if self.iyr is None:
-            return False
-        return True
+        if self.iyr:
+            iyr_int = int(self.iyr)
+        else:
+            iyr_int = 0
+        conditions = [
+            self.iyr,
+            2010 <= iyr_int <= 2020
+        ]
+        return all(conditions)
 
     def valid_eyr(self):
-        if self.eyr is None:
-            return False
-        return True
+        if self.eyr:
+            eyr_int = int(self.eyr)
+        else:
+            eyr_int = 0
+        conditions = [
+            self.eyr,
+            2020 <= eyr_int <= 2030
+        ]
+        return all(conditions)
 
     def valid_hgt(self):
-        if self.hgt is None:
+        if not self.hgt or len(self.hgt) <= 3:
             return False
-        return True
+        value_format = self.hgt[-2:]
+        length = len(self.hgt)
+        value = int(self.hgt[:length -2])
+        if value_format == 'cm':
+            return 150 <= value <= 193
+        if value_format == 'in':
+            return 59 <= value <= 76
+        return False
 
     def valid_hcl(self):
-        if self.hcl is None:
+        if not self.hcl or self.hcl[0] != '#' or len(self.hcl_string) != 6:
             return False
-        return True
+        return self._hcl_contains_valid_characters()
+
+    def _hcl_contains_valid_characters(self):
+        valid_characters = re.compile(r'[^a-f0-9]')
+        string = valid_characters.search(self.hcl_string)
+        return not bool(string)
 
     def valid_ecl(self):
-        if self.ecl is None:
-            return False
-        return True
+        valid_colors = [
+            "amb",
+            "blu",
+            "brn",
+            "gry",
+            "grn",
+            "hzl",
+            "oth"
+        ]
+        if self.ecl and self.ecl in valid_colors:
+            return True
+        return False
 
     def valid_pid(self):
-        if self.pid is None:
-            return False
+        if self.pid and len(self.pid) == 9 and self._pid_contains_valid_characters():
+            return True
         return True
+
+    def _pid_contains_valid_characters(self):
+        valid_characters = re.compile(r'[^0-9]')
+        string = valid_characters.search(self.pid)
+        return not bool(string)
+
+    def print_passport(self):
+        print(f'{self.byr=} {self.valid_byr()=} {self.iyr=} {self.valid_iyr()=} {self.eyr=} {self.valid_eyr()=} {self.hgt=} {self.valid_hgt()=} {self.hcl=} {self.valid_hcl()=} {self.ecl=} {self.valid_ecl()=} {self.pid=} {self.valid_pid()=}')
 
 
 def part1():
-    optional_keys = ['cid']
     print(_get_valid_passport_count('input/day_04'))
     # answer: 196
 
 
 def part2():
-    _get_valid_passport_count('input/day_04_ex')
+    print(_get_valid_passport_count('input/day_04_ex_8'))
 
 
 def _get_valid_passport_count(input_file):
@@ -95,6 +145,8 @@ def _get_valid_passport_count(input_file):
     for passport in passports:
         if passport.is_valid():
             valid_passport_count += 1
+        passport.print_passport()
+        input("Press Enter to continue...")
     return valid_passport_count
 
 
