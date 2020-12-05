765cfc9644fa9c4ed082102208bbd058b4b36a24
Steven Powell <princeodd@gmail.com>
2020-12-05 16:44:44 +0000

    solved 20d5p2

--- aoc2020/aoc2020/day_05.py
+++ aoc2020/aoc2020/day_05.py
@@ -1,6 +1,13 @@
+import logging
+import math
 import statistics
 
 from common.read_file import get_input_as_strings
+
+logger = logging.getLogger(__name__)
+logging_fh = logging.FileHandler('debug.log', mode='w')
+logger.addHandler(logging_fh)
+logger.setLevel(logging.DEBUG)
 
 
 class Plane():
@@ -50,18 +57,26 @@ def _get_seat_num(input_file):
         col = line[-3:]
         # print(f'{line=} {row=} {col=}')
         plane = Plane(row, 'F', 'B', 0, 128)
-        row_num = plane.get_position()
+        row_num = math.trunc(plane.get_position())
         # print(f'{row_num=}')
         plane = Plane(col, 'L', 'R', 0, 8)
-        col_num = plane.get_position()
+        col_num = math.trunc(plane.get_position())
         # print(f'{col_num=}')
         seat_id = _calculate_seat_id(row_num, col_num)
         seat_ids.append(seat_id)
         # print(f'{row_num=} {col_num=} {seat_id=}')
         coordinates.append((row_num, col_num))
     # print(coordinates)
-    # coordinates.sort(key=lambda tup: tup[0])
-    print(len(_get_missing_coordinates(coordinates)))
+    coordinates.sort(key=lambda tup: (tup[0], tup[1]))
+    begin_row = coordinates[0][0]
+    end_row = coordinates[-1][0]
+    for c in coordinates:
+        logger.debug(c)
+    missing = _get_missing_coordinates(coordinates)
+    your_seat = _find_missing_seat(begin_row, end_row, missing)
+    if len(your_seat) == 1:
+        print(f'seat_id: {_calculate_seat_id(your_seat[0][0], your_seat[0][1])}')
+    # print(f'{missing=}')
     return max(seat_ids)
 
 
@@ -79,10 +94,18 @@ def _get_missing_coordinates(coordinates):
         while current[1] <= 7:
             # print(f'{current=}')
             if (current[0], current[1]) not in coordinates:
-                missing.append(current)
+                missing.append(tuple(current))
             current[1] += 1
             # input("Press Enter to continue...")
         current[0] += 1
         current[1] = 0
         # print(f'{current=}')
     return missing
+
+
+def _find_missing_seat(begin_row, end_row, missing):
+    not_end_rows = []
+    for m in missing:
+        if begin_row < m[0] < end_row:
+            not_end_rows.append(m)
+    return not_end_rows
