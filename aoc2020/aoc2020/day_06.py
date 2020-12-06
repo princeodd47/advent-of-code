7aef48848fddd41e04297b1c38688dec1190debc
Steven Powell <princeodd@gmail.com>
2020-12-06 20:46:42 +0000

    completed 20d6

--- aoc2020/aoc2020/day_06.py
+++ aoc2020/aoc2020/day_06.py
@@ -1,14 +1,50 @@
-from common.read_file import get_input
+from common.read_file import get_input_as_strings
 
 
 def part1():
-    print(do_the_thing("input/day_01"))
+    print(anyone_answered_yes("input/day_06"))
+    # answer: 6249
 
 
 def part2():
-    print(do_the_thing("input/day_01"))
+    print(everyone_answered_yes("input/day_06"))
+    # answer: 3103
 
 
-def do_the_thing(input_file):
-    # values = get_input(input_file)
-    return True
+def anyone_answered_yes(input_file):
+    group_answers = []
+    lines = get_input_as_strings(input_file)
+    answers = ''
+    for line in lines:
+        if line != '':
+            if answers == '':
+                answers = line
+            else:
+                answers += line
+        else:
+            group_answers.append(set(answers))
+            answers = ''
+
+    group_answers.append(set(answers))
+    count = 0
+    for a in group_answers:
+        count += len(a)
+    return count
+
+
+def everyone_answered_yes(input_file):
+    group_answers = []
+    lines = get_input_as_strings(input_file)
+    answers = []
+    for line in lines:
+        if line != '':
+            answers.append(set(line))
+        else:
+            group_answers.append(set.intersection(*answers))
+            answers.clear()
+
+    group_answers.append(set.intersection(*answers))
+    count = 0
+    for a in group_answers:
+        count += len(a)
+    return count
