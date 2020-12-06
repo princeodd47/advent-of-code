5e40f56d6fa5ff4ee85d20fa51d5b13ab318eccf
Steven Powell <princeodd@gmail.com>
2020-12-06 19:53:44 +0000

    scaffold 20d6

---
+++ aoc2020/aoc2020/day_06.py
@@ -0,0 +1,14 @@
+from common.read_file import get_input
+
+
+def part1():
+    print(do_the_thing("input/day_01"))
+
+
+def part2():
+    print(do_the_thing("input/day_01"))
+
+
+def do_the_thing(input_file):
+    # values = get_input(input_file)
+    return True
