9174de9548d15c11745f5cc2285b186f4f90e2b6
Steven Powell <princeodd@gmail.com>
2020-12-07 15:46:35 +0000

    completed 20d7p1

--- aoc2020/aoc2020/day_07.py
+++ aoc2020/aoc2020/day_07.py
@@ -1,14 +1,73 @@
+import json
+import re
+
 from common.read_file import get_input_as_strings
 
 
+class Bag:
+    def __init__(self, name, contents):
+        self.name = name
+        self.contents = contents
+
+
+class Rules:
+    def __init__(self, rules):
+        self.rules = rules
+        self.parent_bags = []
+
+    def get_parent_bags(self, desired_bag):
+        temp_parents = []
+        for r in self.rules:
+            if desired_bag in r.contents:
+                # print(r.name, r.contents)
+                self.parent_bags.append(r)
+                self.get_parent_bags(r.name)
+
+
 def part1():
-    print(do_the_thing("input/day_07"))
+    desired_bag = 'shiny gold'
+    print(_get_max_bag_count("input/day_07", desired_bag))
 
 
 def part2():
-    print(do_the_thing("input/day_07"))
+    print(_do_the_thing("input/day_07"))
 
 
-def do_the_thing(input_file):
-    # lines = get_input_as_strings(input_file)
+def _get_max_bag_count(input_file, desired_bag):
+    rules = Rules(_parse_input(input_file))
+    rules.get_parent_bags(desired_bag)
+    print('parent bags')
+    # for b in rules.parent_bags:
+    #     print(b.name, b.contents)
+    return len(set(rules.parent_bags))
+
+
+def _parse_input(input_file):
+    rules = []
+    lines = get_input_as_strings(input_file)
+    for line in lines:
+        split_string = line.split(' bags contain ')
+        bag_name = split_string[0]
+        contents= _parse_contents(split_string[1])
+        bag = Bag(bag_name, contents)
+        rules.append(bag)
+    return rules
+
+def _parse_contents(string):
+    string = string.replace('bags', 'bag')
+    string = string.replace(' bag, ', ',')
+    string = string.replace(' bag.', '')
+    if string == "no other":
+        return {}
+    split_string = string.split(',')
+    contents = {}
+    for s in split_string:
+        s_split = s.split()
+        count = s_split[0]
+        bag = re.sub(r'[0-9] ', '', s)
+        contents[bag] = count
+    return contents
+
+
+def _do_the_thing(input_file):
     return True
