7d399bd0f4c9c041e2d2b62226097b186f2dda2a
Steven Powell <princeodd@gmail.com>
2020-12-07 13:16:13 +0000

    scaffold 20d7

---
+++ aoc2020/aoc2020/day_07.py
@@ -0,0 +1,14 @@
+from common.read_file import get_input_as_strings
+
+
+def part1():
+    print(do_the_thing("input/day_07"))
+
+
+def part2():
+    print(do_the_thing("input/day_07"))
+
+
+def do_the_thing(input_file):
+    # lines = get_input_as_strings(input_file)
+    return True
