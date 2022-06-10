9ca2d010059e29b74b93e6ebaf8fcb99c957a161
Steven Powell <princeodd@gmail.com>
2022-06-10 17:18:14 -0500

    2015 d1p2 solved

--- aoc2015/aoc2015/day_01.py
+++ aoc2015/aoc2015/day_01.py
@@ -21,3 +21,23 @@ def _traverse_floors(starting_floor, instructions):
         elif i == ")":
             current_floor -= 1
     return current_floor
+
+
+def part2():
+    instructions = _get_instructions("input/day_01")
+    starting_floor = 0
+    position = _find_first_basement_position(starting_floor, instructions)
+    print(position)
+
+
+def _find_first_basement_position(starting_floor, instructions):
+    current_position = 0
+    current_floor = starting_floor
+    for i in instructions:
+        if i == "(":
+            current_floor += 1
+        elif i == ")":
+            current_floor -= 1
+        current_position += 1
+        if current_floor < 0:
+            return current_position
--- aoc2015/tests/test_day_01.py
+++ aoc2015/tests/test_day_01.py
@@ -19,3 +19,15 @@ from aoc2015 import day_01
 def test_part1(input_string, expected_result):
     return_value = day_01._traverse_floors(0, input_string)
     assert return_value == expected_result
+
+
+@pytest.mark.parametrize(
+    ("input_string", "expected_result"),
+    [
+        (")", 1),
+        ("()())", 5),
+    ]
+)
+def test_part2(input_string, expected_result):
+    return_value = day_01._find_first_basement_position(0, input_string)
+    assert return_value == expected_result
